dfccc9b9754bd4590d3f2ad7bb72c5d324b4edb1
islombek <abduganiyev>
2026-05-08 19:32:42 +0500

    create anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import LandingPage from "./Components/Content";
 
 function App() {
   return (
     <div>
-      
+      <LandingPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
